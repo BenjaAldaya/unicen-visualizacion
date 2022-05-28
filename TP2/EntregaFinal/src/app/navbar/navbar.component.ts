4e801f22b7c86fe37626d2781088c55bb787ba4a
facubinetti <facubinetti.96@gmail.com>
2022-05-28 14:09:11 -0300

    categorias desde arreglo y mejora visual

--- TP2/EntregaFinal/src/app/navbar/navbar.component.ts
+++ TP2/EntregaFinal/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../category';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,52 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  Categorias : Category[] = [
+    {
+      name: 'Acción',
+      icon:'./assets/images/category/icon-accion.svg',
+      link:'accion'
+    },
+    {
+      name: 'Multijugador',
+      icon:'./assets/images/category/icon-.svg',
+      link:'multijugador'
+    },
+    {
+      name: 'Casino',
+      icon:'./assets/images/category/icon-.svg',
+      link:'casino'
+    },
+    {
+      name: 'Aventura',
+      icon:'./assets/images/category/icon-.svg',
+      link:'aventura'
+    },
+    {
+      name: 'de Mesa',
+      icon:'./assets/images/category/icon-.svg',
+      link:'juegos-de-mesa'
+    },
+    {
+      name: 'Simulación',
+      icon:'./assets/images/category/icon-.svg',
+      link:'simulacion'
+    },
+    {
+      name: 'Rompecabezas',
+      icon:'./assets/images/category/icon-.svg',
+      link:'rompecabezas'
+    },
+    {
+      name: 'Clasicos',
+      icon:'./assets/images/category/icon-.svg',
+      link:'clasicos'
+    },
+    {
+      name: 'Carreras',
+      icon:'./assets/images/category/icon-.svg',
+      link:'carreras'
+    },  
+  ]
 }
